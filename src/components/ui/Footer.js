b40cdf1b362e8391678fbfa42661bd6befe79b1d
Kohina <pandeykohina@gmail.com>
2022-02-16 21:42:59 +0530

    first commit

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='center'>
+      <br/>
+        <span>Created By <b>Kohina Pandey</b></span>
+    </footer>
+  )
+}
+
+export default Footer
